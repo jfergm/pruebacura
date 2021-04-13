1ec04a54aefc600a6f00cfc83676ba5f193e96d0
José Fernando <jose.gomezm12@gmail.com>
2021-04-13 12:31:10 -0500

    database connection

--- app.py
+++ app.py
@@ -3,8 +3,30 @@ from routes.estados import estados_bp
 from routes.municipios import municipios_bp
 from routes.colonias import colonias_bp
 from routes.tipo_asentamientos import tipo_asentamientos_bp
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+from database import db
 
 app = Flask(__name__)
+
+def create_connection_string():
+  type = os.environ.get('DATABASE_TYPE')
+  host = os.environ.get('DATABASE_HOST')
+  port = os.environ.get('DATABASE_PORT')
+  name = os.environ.get('DATABASE_NAME')
+  user = os.environ.get('DATABASE_USER')
+  password = os.environ.get('DATABASE_PASSWORD')
+
+  url = f'{type}://{user}:{password}@{host}:{port}/{name}'
+  print(url)
+  return url
+
+app.config['SQLALCHEMY_DATABASE_URI'] = create_connection_string()
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
 
 @app.route('/')
 def index():
@@ -17,3 +39,5 @@ app.register_blueprint(tipo_asentamientos_bp, url_prefix='/api/tipo_asentamiento
 
 if __name__ == '__main__':
   app.run(debug = True, host = '0.0.0.0')
+
+
