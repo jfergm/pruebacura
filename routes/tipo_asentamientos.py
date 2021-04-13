2fc1267e5f90a7db7669969b17b643984bfc9731
José Fernando <jose.gomezm12@gmail.com>
2021-04-13 14:11:38 -0500

    actions

--- routes/tipo_asentamientos.py
+++ routes/tipo_asentamientos.py
@@ -1,19 +1,19 @@
 from flask import Blueprint, request
-
+from controllers.tipo_asentamientos_controller import *
 tipo_asentamientos_bp = Blueprint('tipos_asentamiento', __name__)
 
 @tipo_asentamientos_bp.route('/', methods=['GET', 'POST'])
 def municipios():
   if(request.method == 'GET'):
-    return ''
+    return obtener_tipo_asentamientos()
   elif(request.method == 'POST'):
-    return ''
+    return crear_tipo_asentamiento(request.get_json())
 
 @tipo_asentamientos_bp.route('/<id>', methods=['GET', 'PATCH', 'DELETE'])
 def municipio(id):
   if(request.method == 'GET'):
-    return ''
+    return obtener_tipo_asentamiento(id)
   elif(request.method == 'PATCH'):
-    return ''
+    return actualizar_tipo_asentamiento(id, request.get_json())
   elif(request.method == 'DELETE'):
-    return ''
+    return eliminar_tipo_asentamiento(id)
