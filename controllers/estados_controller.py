2f76a978449972fe4f315b07dce50b1d21d1c983
José Fernando <jose.gomezm12@gmail.com>
2021-04-13 15:38:55 -0500

    query by name

--- controllers/estados_controller.py
+++ controllers/estados_controller.py
@@ -2,7 +2,10 @@ from app import db
 from models.estado import Estado
 from flask import jsonify
 
-def obtener_estados():
+def obtener_estados(query):
+  if(query.get('name') and query.get('name') != ''):
+    return jsonify([e.serialize for e in Estado.query.filter_by(d_estado=query.get('name'))])
+  
   return jsonify([e.serialize for e in Estado.query.all()])
 
 def obtener_estado(id):
@@ -18,7 +21,7 @@ def crear_estado(estado):
 def actualizar_estado(id, estado_data):
   estado = Estado.query.get(id)
 
-  estado.d_estado = estado_data['d_estado'] if estado_data['d_estado'] else estado.d_estado
+  estado.d_estado = estado_data['d_estado'] if 'd_estado' in estado_data.keys() else estado.d_estado
 
   db.session.commit()
 
