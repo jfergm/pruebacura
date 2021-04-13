a0f977ef013d9e5781036fdabeaec79045691133
José Fernando <jose.gomezm12@gmail.com>
2021-04-13 12:03:46 -0500

    register routes

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask
+from routes.estados import estados_bp
+from routes.municipios import municipios_bp
+from routes.colonias import colonias_bp
+from routes.tipo_asentamientos import tipo_asentamientos_bp
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
   return 'Hello world'
+
+app.register_blueprint(estados_bp, url_prefix='/api/estados/')
+app.register_blueprint(municipios_bp, url_prefix='/api/municipios/')
+app.register_blueprint(colonias_bp, url_prefix='/api/colonias/')
+app.register_blueprint(tipo_asentamientos_bp, url_prefix='/api/tipo_asentamientos')
 
 if __name__ == '__main__':
   app.run(debug = True, host = '0.0.0.0')
